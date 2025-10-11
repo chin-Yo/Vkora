498954ee46a10102d09dea187e24724ff12957ab
chinYo <104709525+chin-Yo@users.noreply.github.com>
2025-10-11 17:55:21 +0800

    优化代码结构 (#5)

--- Engine/Source/Runtime/Engine/Source/SceneGraph/Components/PerspectiveCamera.cpp
+++ Engine/Source/Runtime/Engine/Source/SceneGraph/Components/PerspectiveCamera.cpp
@@ -22,56 +22,57 @@
 
 namespace vkb
 {
-namespace sg
-{
-PerspectiveCamera::PerspectiveCamera(const std::string &name) :
-    Camera{name}
-{}
+    namespace sg
+    {
+        PerspectiveCamera::PerspectiveCamera(const std::string& name) :
+            Camera{name}
+        {
+        }
 
-void PerspectiveCamera::set_field_of_view(float new_fov)
-{
-	fov = new_fov;
-}
+        void PerspectiveCamera::set_field_of_view(float new_fov)
+        {
+            fov = new_fov;
+        }
 
-float PerspectiveCamera::get_far_plane() const
-{
-	return far_plane;
-}
+        float PerspectiveCamera::get_far_plane() const
+        {
+            return far_plane;
+        }
 
-void PerspectiveCamera::set_far_plane(float zfar)
-{
-	far_plane = zfar;
-}
+        void PerspectiveCamera::set_far_plane(float zfar)
+        {
+            far_plane = zfar;
+        }
 
-float PerspectiveCamera::get_near_plane() const
-{
-	return near_plane;
-}
+        float PerspectiveCamera::get_near_plane() const
+        {
+            return near_plane;
+        }
 
-void PerspectiveCamera::set_near_plane(float znear)
-{
-	near_plane = znear;
-}
+        void PerspectiveCamera::set_near_plane(float znear)
+        {
+            near_plane = znear;
+        }
 
-void PerspectiveCamera::set_aspect_ratio(float new_aspect_ratio)
-{
-	aspect_ratio = new_aspect_ratio;
-}
+        void PerspectiveCamera::set_aspect_ratio(float new_aspect_ratio)
+        {
+            aspect_ratio = new_aspect_ratio;
+        }
 
-float PerspectiveCamera::get_field_of_view()
-{
-	return fov;
-}
+        float PerspectiveCamera::get_field_of_view()
+        {
+            return fov;
+        }
 
-float PerspectiveCamera::get_aspect_ratio()
-{
-	return aspect_ratio;
-}
+        float PerspectiveCamera::get_aspect_ratio()
+        {
+            return aspect_ratio;
+        }
 
-glm::mat4 PerspectiveCamera::get_projection()
-{
-	// Note: Using reversed depth-buffer for increased precision, so Znear and Zfar are flipped
-	return glm::perspective(fov, aspect_ratio, far_plane, near_plane);
-}
-}        // namespace sg
-}        // namespace vkb
+        glm::mat4 PerspectiveCamera::get_projection()
+        {
+            // TODO: Using reversed depth-buffer for increased precision, so Znear and Zfar are flipped
+            return glm::perspective(fov, aspect_ratio, far_plane, near_plane);
+        }
+    } // namespace sg
+} // namespace vkb
